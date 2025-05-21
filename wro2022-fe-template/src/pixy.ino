6d36ec480d8ac7543fc8229c4a9d122e050cb2ab
gaby-FI <gabrielanoemicisneros@gmail.com>
2025-05-21 12:32:02 -0400

    Add files via upload

---
+++ wro2022-fe-template/src/pixy.ino
@@ -0,0 +1,102 @@
+
+// Librerias
+  #include <PIDLoop.h>
+  #include <Pixy2.h>
+  #include <Pixy2CCC.h>
+  #include <Pixy2I2C.h>
+  #include <Pixy2Line.h>
+  #include <Pixy2SPI_SS.h>
+  #include <Pixy2UART.h>
+  #include <Pixy2Video.h>
+  #include <TPixy2.h>
+  #include <ZumoBuzzer.h>
+  #include <ZumoMotors.h>
+  #include <Arduino.h>
+  #include <SPI.h>
+  #include <Wire.h>
+  #include <I2Cdev.h>
+  #include <string.h>
+  #include <Servo.h>
+    Servo servito;
+// Definir variables
+  #define num
+  #define ancho
+  #define rojo
+  #define verde
+// Velocidad
+  #define Activa1 6
+  #define Activa2 5
+// Motor
+  #define MOTOR_IZQ_ADELANTE 11
+  #define MOTOR_IZQ_ATRAS 9
+  #define MOTOR_DER_ADELANTE 8
+  #define MOTOR_DER_ATRAS 7
+// Ultrasonidos
+  #define trigpin1 5
+  #define echopin1 4
+  #define trigpin2 3
+  #define echopin2 2
+// Velocidad
+  int velocidad=200;
+
+// Temporizador
+ unsigned long TiempoInicio=millis();
+ const unsigned long DuracionTotal=180000;
+
+
+
+  void girarDerecha() {
+  digitalWrite(MOTOR_IZQ_ADELANTE, 0);
+  digitalWrite(MOTOR_IZQ_ATRAS, 0);
+  digitalWrite(MOTOR_DER_ADELANTE, 1);
+  digitalWrite(MOTOR_DER_ATRAS, 1);
+    delay(500); // Ajusta el tiempo de giro según sea necesario
+}
+void girarIzquierda() {
+  digitalWrite(MOTOR_IZQ_ADELANTE, 1);
+  digitalWrite(MOTOR_IZQ_ATRAS, 1);
+  digitalWrite(MOTOR_DER_ADELANTE, 0);
+  digitalWrite(MOTOR_DER_ATRAS, 0);
+    delay(500); // Ajusta el tiempo de giro según sea necesario
+}
+
+
+
+void setup() {
+  serial.begin(9600);
+  pixy.init();
+  pixy.setLamp(1,1); // Encender luces de la camara
+  int servito;
+    rojo=pixy.ccc.Block[0].m_signature==1; // Definir rojo
+    verde=pixy.ccc.Block[0].m_signature==2; // Definir verde
+    ancho=pixy.ccc.Block[0].m_width; // Definir ancho
+}
+void loop() {
+  num=pixy.getblocks();
+  millis ()-TiempoInicio
+
+do{
+  if(pixy.ccc.Block>0 && rojo){
+    if(ancho>60){ // 3cm
+      servito.write(180);    
+    }
+  }else{
+    if(pixy.ccc.Block>0 && verde){
+      if(ancho>60){ // 3cm
+        servito.write(0); 
+      }
+    }
+  }
+} while(TiempoInicio<=DuracionTotal);
+  pixy.setLamp(0,0); // Apagar luces de la camara
+
+/*
+  num=pixy.getblocks();
+  if(num>0){
+    if(ancho=pixy.blocks[0].width){
+      
+    }
+  }
+*/
+
+}
